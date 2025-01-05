e818fd21df091fa220e80d1fc5cf8e071c8b05a5
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-05 20:58:03 -0300

    Agregar vistas InicioView y FavoritosView, y actualizar App.vue para incluir RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 import HeaderComp from './components/HeaderComp.vue';
+import { RouterView } from 'vue-router';
+
 </script>
 
 <template>
   <HeaderComp />
+
+  <main class="container mx-auto mt-10">
+    <RouterView />
+  </main>
 </template>
