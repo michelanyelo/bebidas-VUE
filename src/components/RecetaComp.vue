aa217e38c9daf9bed5d6063b7a1e65a2b30a7773
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-06 13:23:51 -0300

    Agregar componente RecetaComp y mostrar recetas en InicioView

---
+++ src/components/RecetaComp.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  receta: {
+    type: Object,
+  }
+})
+</script>
+
+<template>
+  <div class="border shadow-lg">
+    <div class="overflow-hidden">
+      <img :src="receta.strDrinkThumb" :alt="receta.strDrink"
+        class="rounded-t-lg w-full hover:scale-125 transition-transform duration-500 hover:rotate-2" />
+    </div>
+    <div class="p-5">
+      <h2 class="text-2xl font-extrabold truncate">{{ receta.strDrink }}</h2>
+      <button type="button"
+        class="bg-orange-800 hover:bg-orange-900 mt-5 w-full p-3 font-bold text-white text-sm uppercase rounded-lg">Ver
+        receta</button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,9 +1,16 @@
 <script setup>
+import { useBebidasStore } from '@/stores/bebidasStore.js';
+import RecetaComp from '@/components/RecetaComp.vue';
 
+const bebidaStore = useBebidasStore();
 </script>
 
 <template>
   <h1 class="text-6xl font-extrabold">Recetas</h1>
+
+  <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+    <RecetaComp v-for="receta in bebidaStore.recetas" :key="receta.idDrink" :receta="receta" />
+  </div>
 </template>
 
 <style scoped></style>
